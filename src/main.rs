394d1948ba29533ec4e29dd3ec24f9c14be12eb9
Sumod Sivadas <sumodn@mashreq.com>
2024-01-10 22:16:32 +0400

    Created the UI with Slint

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });
 
     ui.run()
 }
